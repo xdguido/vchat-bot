ee9ad598857551ea634990cf451211089bda0c35
xdguido <guidogennari95@gmail.com>
2025-01-27 16:47:58 -0300

    feat: add ollama chat

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,22 @@
+import { streamText } from 'ai';
+import { createOllama } from 'ollama-ai-provider';
+
+const ollama = createOllama({
+  // optional settings, e.g.
+  baseURL: 'http://localhost:11434/api',
+});
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: ollama('llama3.2:1b'),
+    system: 'give very concise answers.',
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}
